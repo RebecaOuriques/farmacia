6936bc0928a26d0fd8043548617c74de15876bbc
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-16 15:30:08 -0300

    Fix cor indigo

--- src/componentes/categoria/cardCategoria/CardCategoria.tsx
+++ src/componentes/categoria/cardCategoria/CardCategoria.tsx
@@ -13,7 +13,7 @@ function CardCategoria({categoria} : CardCategoriaProps) {
       <header className='py-2 px-6 bg-blue-900 text-white font-bold text-2xl'>Categoria - {categoria.nome}</header>
       <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
       <div className="flex">
-        <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-slate-100 bg-indigo-400 hover:bg-blue-900 flex items-center justify-center py-2'>
+        <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-slate-100 bg-blue-600 hover:bg-blue-900 flex items-center justify-center py-2'>
           <button>Editar</button>
         </Link>
         <Link to={`/deletarCategoria/${categoria.id}`} className='text-slate-100 bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
--- src/componentes/categoria/formCategoria/FormCategoria.tsx
+++ src/componentes/categoria/formCategoria/FormCategoria.tsx
@@ -97,7 +97,7 @@ return (
         />
       </div>
       <button
-        className="rounded text-slate-100 bg-indigo-400 hover:bg-indigo-800 w-1/2 py-2 mx-auto block"
+        className="rounded text-slate-100 bg-blue-600 hover:bg-blue-900 w-1/2 py-2 mx-auto block"
         type="submit"
       >
         {id === undefined ? 'Cadastrar' : 'Editar'}
